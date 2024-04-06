d82b6141f0c91c1fcf969ab0ab288c4ecf028f18
arsenalist777 <arsenalist777@gmail.com>
2024-04-06 17:09:53 +0900

    add xG against chats

--- js/data/expected-against.js
+++ js/data/expected-against.js
@@ -7,6 +7,8 @@ class ExpectedAgainst {
         let expectedAgainstData = {
             'shots_xg': [],
             'spshots_spxG': [],
+            'xG_G-xG': [],
+            'spxG_spG-spxG': [],
         };
         Object.keys(data).map(key => {
             expectedAgainstData['shots_xg'].push({
@@ -17,6 +19,16 @@ class ExpectedAgainst {
             expectedAgainstData['spshots_spxG'].push({
                 'spShots': Number(data[key][Const.EXPECTED_GAOLS_AGAINST.SP_SHOTS_COL]),
                 'spxG': Number(data[key][Const.EXPECTED_GAOLS_AGAINST.SP_XG_COL]),
+                'img': Const.IMG_URL[data[key][Const.EXPECTED_GAOLS_AGAINST.SQUAD_COL]]
+            });
+            expectedAgainstData['xG_G-xG'].push({
+                'xG': Number(data[key][Const.EXPECTED_GAOLS_AGAINST.OP_XG_COL]) + Number(data[key][Const.EXPECTED_GAOLS_AGAINST.SP_XG_COL]),
+                'G-xG': (Number(data[key][Const.EXPECTED_GAOLS_AGAINST.OP_GOALS_COL]) + Number(data[key][Const.EXPECTED_GAOLS_AGAINST.SP_GOALS_COL])) - ((Number(data[key][Const.EXPECTED_GAOLS_AGAINST.OP_XG_COL]) + Number(data[key][Const.EXPECTED_GAOLS_AGAINST.SP_XG_COL]))),
+                'img': Const.IMG_URL[data[key][Const.EXPECTED_GAOLS_AGAINST.SQUAD_COL]]
+            });
+            expectedAgainstData['spxG_spG-spxG'].push({
+                'spxG': Number(data[key][Const.EXPECTED_GAOLS_AGAINST.SP_XG_COL]),
+                'spG-spxG': Number(data[key][Const.EXPECTED_GAOLS_AGAINST.SP_GOALS_COL]) - Number(data[key][Const.EXPECTED_GAOLS_AGAINST.SP_XG_COL]),
                 'img': Const.IMG_URL[data[key][Const.EXPECTED_GAOLS_AGAINST.SQUAD_COL]]
             });
         });
